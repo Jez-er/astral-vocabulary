d70b547e2456fc10e012d09bcb368b4dde993740
Jez-er <andrey.chernenko008@gmail.com>
2025-02-18 22:36:55 +0200

    [ADD] Header

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,10 +1,13 @@
 import { Outlet } from 'react-router'
+import Header from '../shared/components/header/Header'
 
 const MainLayout = () => {
 	return (
 		<>
 			<section className='w-screen h-screen bg-black/93 text-white'>
-				<header className='w-screen h-auto'>ffff</header>
+				<header className='w-screen h-auto'>
+					<Header />
+				</header>
 				<main className='w-screen h-auto'>
 					<Outlet />
 				</main>
---
+++ src/shared/components/header/Header.tsx
@@ -0,0 +1,30 @@
+import { NavLink } from 'react-router'
+import { Button } from '../../ui/button'
+import Logo from '../logo'
+import { HeaderData } from './data'
+
+const Header = () => {
+	return (
+		<section className='px-52 flex h-full w-full items-center justify-around border-b-2 border-gray-400 py-3.5'>
+			<div>
+				<Logo WithText />
+			</div>
+			<nav className='flex gap-5'>
+				{HeaderData.map((data, index) => (
+					<NavLink
+						className='hover:text-violet-500 hover:underline transition-all duration-300'
+						to={data.link}
+						key={index}
+					>
+						{data.title}
+					</NavLink>
+				))}
+			</nav>
+			<div>
+				<Button className='cursor-pointer'>Profile</Button>
+			</div>
+		</section>
+	)
+}
+
+export default Header
