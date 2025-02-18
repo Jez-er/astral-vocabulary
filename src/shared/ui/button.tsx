6959878f6f63d9713c090cd5c149ed258dcf43e7
Jez-er <andrey.chernenko008@gmail.com>
2025-02-18 22:58:39 +0200

    [FIX] Button style

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -5,12 +5,12 @@ import * as React from 'react'
 import { cn } from '../../lib/utils'
 
 const buttonVariants = cva(
-	'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-lg text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300 duration-300',
+	'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-xl text-sm font-medium ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-neutral-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 dark:ring-offset-neutral-950 dark:focus-visible:ring-neutral-300 transition-all duration-500',
 	{
 		variants: {
 			variant: {
 				default:
-					'bg-neutral-900 text-neutral-50 hover:bg-violet-500 dark:bg-neutral-50 dark:text-neutral-900 dark:hover:bg-violet-500 ',
+					'bg-neutral-900 text-neutral-50 hover:bg-violet-500 dark:bg-neutral-50/80 dark:text-neutral-900 dark:hover:bg-violet-500 dark:hover:text-white/90',
 				destructive:
 					'bg-red-500 text-neutral-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-neutral-50 dark:hover:bg-red-900/90',
 				outline:
@@ -20,9 +20,10 @@ const buttonVariants = cva(
 				ghost:
 					'hover:bg-neutral-100 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50',
 				link: 'text-neutral-900 underline-offset-4 hover:underline dark:text-neutral-50',
+				border: 'border-2 border-violet-500 text-white/90 hover:bg-violet-500',
 			},
 			size: {
-				default: 'h-10 px-4 py-2',
+				default: 'h-10',
 				sm: 'h-9 rounded-md px-3',
 				lg: 'h-11 rounded-md px-8',
 				icon: 'h-10 w-10',
